c54b00775af2382dd88e5465d82fbb9078eecdec
Nabilah Argyanti <nargyanti@gmail.com>
2026-06-29 21:08:45 +0700

    Create SectionTitle.vue

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="relative flex items-center">
+        <h2 class="mr-4 flex-shrink text-2xl font-semibold text-gray-900">
+            <slot></slot>
+        </h2>
+        <div class="flex-grow border-4 border-t border-primary"></div>
+    </div>
+</template>
