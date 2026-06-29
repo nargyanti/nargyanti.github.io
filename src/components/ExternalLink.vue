a308299ac9e2e921f5a1df3b2322b2062a8709ea
Nabilah Argyanti <nargyanti@gmail.com>
2026-06-29 20:59:08 +0700

    Create ExternalLink.vue

---
+++ src/components/ExternalLink.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    url: {
+        type: String,
+        required: true
+    },
+})
+</script>
+
+<template>
+    <a :href="url" target="_blank" rel="noopener noreferrer">
+        <slot></slot>
+    </a>
+</template>
