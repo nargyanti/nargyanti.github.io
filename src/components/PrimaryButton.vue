7780a70279c6311b0d75b5c6f99affecf2c54fd1
Nabilah Argyanti <nargyanti@gmail.com>
2026-06-29 21:06:41 +0700

    Create PrimaryButton.vue

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button
+        class="flex items-center rounded-lg border-2 border-primary bg-primary px-4 py-2 font-medium text-white hover:border-primary-dark hover:bg-primary-dark">
+        <slot></slot>
+    </button>
+</template>
