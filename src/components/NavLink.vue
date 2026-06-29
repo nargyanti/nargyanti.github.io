01852cd94691e498cc0732f0630f20cf1f6b9e31
Nabilah Argyanti <nargyanti@gmail.com>
2026-06-29 21:05:53 +0700

    Create NavLink.vue

---
+++ src/components/NavLink.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+defineProps({
+    to: {
+        type: [String, Object],
+        required: true,
+    },
+});
+
+defineEmits(["closeMenu"]);
+</script>
+
+<template>
+    <RouterLink
+        :to="to"
+        @click="$emit('closeMenu')"
+        class="font-semibold transition duration-100 hover:text-primary hover:underline"
+    >
+        <slot></slot>
+    </RouterLink>
+</template>
