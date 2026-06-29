7bc9d5ee6ffb4043438d951ace924dde56984ce2
Nabilah Argyanti <nargyanti@gmail.com>
2026-06-29 21:00:14 +0700

    Create FallbackImage.vue

---
+++ src/components/FallbackImage.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <div class="flex items-center justify-center aspect-video object-cover bg-primary text-white">
+        <p class="text-lg text-center p-8">{{ title }}</p>
+    </div>
+</template>
