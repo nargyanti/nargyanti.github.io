59c47e59836ff9845db4383c23c5eff8844e51cd
Nabilah Argyanti <nargyanti@gmail.com>
2026-06-29 21:08:11 +0700

    Create SecondaryButton.vue

---
+++ src/components/SecondaryButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button
+        class="flex items-center rounded-lg border-2 border-primary bg-white px-4 py-2 font-medium text-primary hover:border-primary-dark hover:bg-primary-dark hover:text-white">
+        <slot></slot>
+    </button>
+</template>
